39daf0c3040cef8a25f5ee60508b72d07b508819
Viderspán Krisztián <krisztian.viderspan@bigfish.hu>
2019-08-30 08:17:56 +0200

    version update

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Version
 
-3.0.1
+3.0.2
 
 ## Requirements
 
--- src/BigFish/PaymentGateway.php
+++ src/BigFish/PaymentGateway.php
@@ -26,7 +26,7 @@ class PaymentGateway
 	/**
 	 * Version
 	 */
-	const VERSION = '3.0.1';
+	const VERSION = '3.0.2';
 
 	/**
 	 * SDK Name
